48636a44eb46d9066ac903b52db72bbd90bbfe0c
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-02-23 17:02:30 -0800

    functions, conditionals, dates and more

---
+++ variable.php
@@ -0,0 +1,73 @@
+<?php
+    // $name = "Hello World";
+    // $num1 = 4;
+    // print $name;
+
+    // for($i=0; $i < 10; $i++) {
+    //     print "Number $i ";
+    //     print '<br>';
+    // }
+
+    // $i = 0;
+
+    // while($i < 10) {
+    //     echo $i;
+    //     echo '<br>';
+    //     $i++;
+    // }
+
+    // $people = ['Brad', 'Jose', 'William'];
+    
+    // foreach($people as $person) {
+    //     echo "My name is $person";
+    //     echo "<br>";
+    // }
+
+    // $people = ['Brad' => 'bigbrad@gmail.com', 'Jose' => 'ItsJose@yahoo.com', 'William' => 'ItsWilliam@gmail.com'];
+
+    // foreach($people as $person => $email){
+    //     echo "$person's email is $email";
+    //     echo "<br>";
+    // }
+
+    // function simplefunc() {
+    //     echo "Hello World";
+    // };
+
+    // simplefunc();
+
+    // function sayHi($name = "dick") {
+    //     echo " Hello $name";
+    // };
+
+    // sayHi();
+
+    // function addNumbers($numb1, $numb2) {
+    //     return $numb1 + $numb2;
+    // }
+
+    // // echo addNumbers(2,3);
+
+
+    // $myNum = 10;
+
+    // function addFive(&$num) {
+    //     $num += 5;
+    // }
+
+    // addFive($myNum);
+
+    // echo "Value: $myNum"
+
+
+        // $num = 5;
+
+        // if($num == 5) {
+        //     echo "balls";
+        // }
+
+        date_default_timezone_set('America/Los_Angeles');
+
+        echo date('h:m:sa');
+
+?>
